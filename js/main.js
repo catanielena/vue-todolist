ff29538b000ebb9931f7de513733161ed6518ae9
catanielena <catani.elena1@gmail.com>
2021-09-20 15:43:04 +0200

    Add: app

--- js/main.js
+++ js/main.js
@@ -3,9 +3,21 @@ const app = new Vue({
     data: {
         todos: [
             'Fare i compiti', 'Fare la spesa', 'Fare il bucato'
-        ]
+        ],
+        newTodo: ""
     },
     methods: {
-        
+        // Add todo to todos
+        addTodo: function() {
+            if(this.newTodo != "") {
+                this.todos.push(this.newTodo);
+                console.log(this.todos);
+                this.newTodo=""
+            }
+        },
+        // Remove todo from todos
+        removeTodo: function(index) {
+            this.todos.splice(index, 1);
+        }
     }
 })

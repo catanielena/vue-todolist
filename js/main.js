54a8a858cdc18f25328678754844d57f687c7ed5
catanielena <catani.elena1@gmail.com>
2021-09-20 15:18:30 +0200

    Scaffolding-html

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el:"#root",
+    data: {
+        todos: [
+            'Fare i compiti', 'Fare la spesa', 'Fare il bucato'
+        ]
+    },
+    methods: {
+        
+    }
+})

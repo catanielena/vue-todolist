9847f3198347e1a56bcac4637729e2189786dcf9
catanielena <catani.elena1@gmail.com>
2021-09-20 17:47:10 +0200

    Add: edit-button, check-button

--- js/main.js
+++ js/main.js
@@ -2,22 +2,55 @@ const app = new Vue({
     el:"#root",
     data: {
         todos: [
-            'Fare i compiti', 'Fare la spesa', 'Fare il bucato'
+            {
+                thingToDo: 'Fare i compiti',
+                textClass: ['item__text']
+            },
+            {
+                thingToDo: 'Fare la spesa',
+                textClass: ['item__text']
+            },
+            {
+                thingToDo: 'Fare il bucato',
+                textClass: ['item__text']
+            }
         ],
-        newTodo: ""
+        newTodo: '',
+        checked : []
     },
     methods: {
         // Add todo to todos
         addTodo: function() {
-            if(this.newTodo != "") {
-                this.todos.push(this.newTodo);
+            const addItem = {
+                thingToDo: '',
+                textClass: ['item__text']
+            }
+            if(this.newTodo != '') {
+                addItem.thingToDo = this.newTodo;
+                this.todos.push(addItem);
                 console.log(this.todos);
-                this.newTodo=""
+                this.newTodo = ''
             }
         },
         // Remove todo from todos
         removeTodo: function(index) {
             this.todos.splice(index, 1);
+        },
+        show: function(index) {
+            console.log(this.checked);
+            if(this.todos[index].textClass.includes('done')) {
+                this.todos[index].textClass = this.todos[index].textClass.filter(e => e!=='done');
+            } else {
+                this.todos[index].textClass.push('done');            }
+        },
+        edit: function(index) {
+            let input = document.getElementsByClassName("item__text");
+            if( input[index].disabled == true) {
+                input[index].disabled = false;
+            } else {
+                input[index].disabled = true
+            }
         }
+
     }
 })
